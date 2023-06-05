ca5d228be04c31c73bfd429f20084578ed3707f2
kolton thompson <mr.kolton@gmail.com>
2023-06-05 11:44:55 -0600

    feat: about me page

---
+++ pages/about-me.tsx
@@ -0,0 +1,49 @@
+import { GetStaticProps } from "next"
+
+type AboutMeProps = {
+  title: string
+  description: string
+  slug: string
+  heroImage?: string
+}
+
+const AboutMe = (props: AboutMeProps) => {
+  const { title, description, slug } = props
+
+  return (
+    <div>
+      <h1>{title} </h1>
+      <h1>{description} </h1>
+      <h1>{slug} </h1>
+    </div>
+  )
+}
+export const getStaticProps: GetStaticProps<AboutMeProps> = async () => {
+  const query = `
+    query AboutMeQuery {
+      blogPage(where: {slug: "about-me"}) {
+        title
+        slug
+        description
+        heroImage {
+          url
+        }
+      }
+    }
+`
+
+  const body = { query }
+  const res = await fetch(process.env.API_ENDPOINT, {
+    method: "POST",
+    body: JSON.stringify(body),
+  })
+  const { data } = await res.json()
+  return {
+    props: {
+      title: data.blogPage.title,
+      description: data.blogPage.description,
+      slug: data.blogPage.slug,
+    },
+  }
+}
+export default AboutMe
