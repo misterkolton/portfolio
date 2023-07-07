541f3ce4b2dda2777d4e8b5d1ed6116572cd5743
kolton thompson <mr.kolton@gmail.com>
2023-07-07 11:11:58 -0600

    chore: I believe that i need to add some files to my git ignore. not sure what these 16 changes are

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "430f70667d207f2e"; }
+/******/ 		__webpack_require__.h = function() { return "6b5c18397ca35250"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.77f50c9722d3faa4.hot-update.js
+++ .next/static/webpack/webpack.77f50c9722d3faa4.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "525be687f7eeb1dc"; }
+/******/ 	__webpack_require__.h = function() { return "6b5c18397ca35250"; }
 /******/ }();
 /******/ 
 /******/ }
